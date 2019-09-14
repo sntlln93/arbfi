ca7185ab3be7a1d8739093c62c720f31109be23e
Matias Santillan <sntlln.93@gmail.com>
2019-09-14 13:10:19 -0300

    still testing

--- app/Helpers/pdffromhtml.php
+++ app/Helpers/pdffromhtml.php
@@ -5,7 +5,7 @@ function generatePDF($players){
 	
 	$view = view('carnets')->with('players', $players)->render();
 	$client->setPageSize("A4");
-	$client->setPageMargins("20mm", "15mm", "20mm", "15mm");
+	$client->setPageMargins("25mm", "15mm", "25mm", "15mm");
 	$pdf = $client->convertString($view);
 	header("Content-Type: application/pdf");
     header("Cache-Control: no-cache");
