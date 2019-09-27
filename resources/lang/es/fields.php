e5e314fe45e4aca7d11d570ac2f076a10c5c4cc2
Matias Santillan <sntlln.93@gmail.com>
2019-09-27 19:49:08 -0300

    this should do

---
+++ resources/lang/es/fields.php
@@ -0,0 +1,34 @@
+<?php
+
+    return [
+        'last_name' => 'apellido',
+        'first_name' => 'nombre',
+        'birth_date' => 'fecha de nacimiento',
+        'team_id' => 'equipo',
+        'image_id' => 'foto',
+        'school' => 'escuela',
+        'position' => 'posición',
+        'prepaid' => 'obra social',
+
+        'name' => 'nombre',
+        'body' => 'contenido',
+        'chapter' => 'capítulo',
+
+        'responsable' => 'responsable',
+        'stadium' => 'cancha',
+
+        'title' => 'título',
+
+        'reason' => 'motivo',
+        'number_matches' => 'cantidad de partidos',
+
+        'article_id' => 'artículo',
+        
+        'type_id' => 'tipo de torneo',
+
+        'username' => 'nombre de usuario',
+        'password' => 'contraseña',
+
+        '_token' => '_token',
+        'image' => 'imagen',
+    ];
